3dca9fef6cbff6db153a4ea43d92ca21ca947717
Nyakamba <omwegaenock@gmail.com>
2024-02-06 14:57:27 +0300

    product and cart models

---
+++ models/products/Product.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const { boolean } = require("webidl-conversions");
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: Array,
+    },
+    size: {
+      type: String,
+    },
+    color: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
