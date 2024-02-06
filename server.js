8c62bd59eb62c858e4a074b59721a3d589c5d68e
Nyakamba <omwegaenock@gmail.com>
2024-02-06 12:53:38 +0300

    creating routes

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-
 require("./config/dbConnect");
+
+//middlewares
+//routes
+app.get("/api/v1/test", () => {
+  console.log("Test is successful");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
