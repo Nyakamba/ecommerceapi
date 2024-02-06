f85add9a9990f06276955d483327d888bb07e6a7
Nyakamba <omwegaenock@gmail.com>
2024-02-06 14:36:17 +0300

    creating models

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const userRoute = require("./routes/users/user");
+
 require("./config/dbConnect");
 
 //middlewares
+app.use(express.json());
 //routes
-app.get("/api/v1/test", () => {
-  console.log("Test is successful");
-});
+app.use("/api/v1/users/", userRoute);
 
+//server listen
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
