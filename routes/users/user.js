8419b7ccb44acd183569c9eaf0a9cd5f81753beb
Nyakamba <omwegaenock@gmail.com>
2024-02-06 17:03:36 +0300

    jwt auhtentication

--- routes/users/user.js
+++ routes/users/user.js
@@ -1,54 +1,12 @@
 const express = require("express");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const { registerCtrl, loginCtrl } = require("../../controllers/user/userCtrl");
 const userRoute = express.Router();
-const User = require("../../models/user/User");
 
 //register
-userRoute.post("/register", async (req, res) => {
-  const { username, email, password } = req.body;
-
-  try {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    const user = await User.create({
-      username,
-      email,
-      password: hashedPassword,
-    });
-    res.json({ data: user });
-  } catch (error) {
-    res.status(500).json(error.message);
-  }
-});
+userRoute.post("/register", registerCtrl);
 
 //login
 
-userRoute.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    return res.json("All fields are required");
-  }
-
-  try {
-    const userFound = await User.findOne({ email });
-
-    if (!userFound) {
-      return res.json("Invalid login credentials");
-    }
-
-    const isPasswordValid = await bcrypt.compare(password, userFound.password);
-
-    if (!isPasswordValid) {
-      return res.json("Invalid login credentials");
-    }
-
-    const accessToken = res.status(200).json(userFound);
-  } catch (error) {
-    res.status(500).json(error.message);
-  }
-});
+userRoute.post("/login", loginCtrl);
 
 module.exports = userRoute;
